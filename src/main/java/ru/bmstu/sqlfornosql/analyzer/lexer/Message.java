842e83468a871009aa492642c1b6bb0f02fc45ce
trishLEX <alexeytr5@gmail.com>
2018-12-29 19:58:25 +0300

    Добавлены классы для позиции и сообщений

---
+++ src/main/java/ru/bmstu/sqlfornosql/analyzer/lexer/Message.java
@@ -0,0 +1,10 @@
+package ru.bmstu.sqlfornosql.analyzer.lexer;
+
+import ru.bmstu.sqlfornosql.analyzer.service.Position;
+
+public class Message {
+    private String text;
+    private Position pos;
+
+
+}
